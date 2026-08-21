1ca2116e1cda4e1b231dfabf6ad694084d7797b8
Gerry-01 <odhiambogerry010@gmail.com>
2026-08-21 14:39:26 +0300

    imported the Signup and Login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
+import SignupPage from "./Pages/SignupPage";
+import LoginPage from "./Pages/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<div>Login Page</div>} />
-        <Route path="/signup" element={<div>Signup Page</div>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route path="/courses" element={<div>Courses Page</div>} />
         <Route path="/dashboard" element={<div>Dashboard</div>} />
       </Routes>
