67742681150c24ceb6e0314975896b31cc7290e5
Gerry-01 <odhiambogerry010@gmail.com>
2026-08-23 09:26:47 +0300

    added the signup serializer

---
+++ Backend/users/serializers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserSignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'password', 'confirm_password']
+
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords do not match.")
+
+        if User.objects.filter(email=data['email']).exists():
+            raise serializers.ValidationError("Email already exists.")
+        return data
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            email=validated_data['email'],
+            username=validated_data['email'],  # Assuming email is used as username
+            password=validated_data['password']
+        )
+        return user
