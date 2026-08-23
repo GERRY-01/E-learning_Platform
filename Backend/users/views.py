5ce0444a3457c0a5afab6b16863dbd94ef18feaa
Gerry-01 <odhiambogerry010@gmail.com>
2026-08-23 11:24:56 +0300

    added the view for login

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -1,7 +1,11 @@
+import email
+
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import UserSignupSerializer
+from .serializers import UserSignupSerializer, UserLoginSerializer
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
 @api_view(['POST'])
@@ -10,4 +14,21 @@ def signup(request):
     if serializer.is_valid():
         serializer.save()
         return Response({'message': 'User created successfully.'}, status=201)
+    return Response(serializer.errors, status=400)
+
+@api_view(['POST'])
+def login_view(request):
+    serializer = UserLoginSerializer(data=request.data)
+    if serializer.is_valid():
+        email = serializer.validated_data['email']
+        password = serializer.validated_data['password']
+        user = authenticate(request, username=email, password=password)
+        if user:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'message': 'Login successful.',
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }, status=200)
+        return Response({'error': 'Invalid credentials.'}, status=401)
     return Response(serializer.errors, status=400)
