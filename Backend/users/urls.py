8f7abc86686f3e134f3ce0e00f5395b3195179e9
Gerry-01 <odhiambogerry010@gmail.com>
2026-08-24 19:03:40 +0300

    add the endpoint for the logged in user api

--- Backend/users/urls.py
+++ Backend/users/urls.py
@@ -1,7 +1,8 @@
-from .views import signup,login_view
+from .views import signup,login_view,get_current_user
 from django.urls import path
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', login_view, name='login'),
+    path('current_user/', get_current_user, name='current_user'),
 ]
